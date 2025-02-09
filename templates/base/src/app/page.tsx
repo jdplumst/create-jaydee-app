8b88918bb9e952898dde749233ed3e7056383f30
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-02-09 11:48:46 -0500

    Change colour of background

--- templates/base/src/app/page.tsx
+++ templates/base/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-purple-600 via-pink-700 to-red-600">
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-black">
       <div className="text-center">
-        <h1 className="mb-4 text-6xl font-bold text-white">
+        <h1 className="mb-4 text-6xl font-bold text-gray-100">
           Create <span className="text-[#FAD]">Jaydee</span> App
         </h1>
         <p className="text-xl text-white">
